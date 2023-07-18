aecf5fb9f9193aed716ac928883645ef8f22be68
hackchele <iancruzcolella@gmail.com>
2023-07-17 23:03:22 -0300

    commit final para render

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-const { log } = require("console");
-const express = require("express");
-const path = require("path");
-const app = express();
-
-app.get("/", (req, res) => {
-    const ruta = path.join(__dirname, "./views/home.html");
-    res.sendFile(ruta);
-});
-
-const publicFolder = path.resolve(__dirname, "./public");
-app.use(express.static(publicFolder));
-
-app.listen(3010, () =>{
-    console.log("El servidor esta escuchando en el 3010");
-});
-
-
-/* 
 const express = require ('express');
 const path = require('path');
 
@@ -26,14 +7,10 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use (express.static(publicPath)); 
 
 const dotenv = require('dotenv').config();
- 
 
- app.listen (process.env.PORT, () => {
+
+app.listen (process.env.PORT, () => {
     console.log('servidor corriendo en el puerto' + process.env.PORT + "http://localhost:" + process.env.PORT)
-}); 
-
-app.listen (3010, () => {
-    console.log('servidor corriendo en el puerto 3010')
 });
 
 app.get("/", (req, res) =>{
@@ -46,4 +23,4 @@ app.get("/register", (req, res) =>{
 
 app.get("/login", (req, res) =>{
     res.sendFile(path.resolve(__dirname, "./views/login.html"))
-}); */
+});
